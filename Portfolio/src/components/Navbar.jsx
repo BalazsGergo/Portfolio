6ad4f2cafe4be7eb17f2a31bb025a2ee1eeecbec
Balazs Gergo <gergobalazs98@gmail.com>
2024-02-12 13:43:32 +0100

    base code added

---
+++ Portfolio/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+       <img src='/public/cat.jpg' alt="image" />
+       <button>Home</button>
+       <button>Tech</button>
+       <button>Projects</button>
+    </div>
+  )
+}
+
+export default Navbar
