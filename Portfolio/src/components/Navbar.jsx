01b02ceebbe9c153aefc659f86153d999c054290
Balazs Gergo <gergobalazs98@gmail.com>
2024-02-13 14:29:24 +0100

    changes added

--- Portfolio/src/components/Navbar.jsx
+++ Portfolio/src/components/Navbar.jsx
@@ -1,12 +1,20 @@
 import './Navbar.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
 
 function Navbar() {
   return (
     <div className="Navbar">
-       <img src='/public/cat.jpg' alt="image" />
+       <h4>Balazs Gergo Frontend Developer</h4>
        <button>Home</button>
        <button>Tech</button>
        <button>Projects</button>
+
+       <FontAwesomeIcon icon={faGithub}/>
+       <FontAwesomeIcon icon={faLinkedin}/>
+   
     </div>
   )
 }
